81433f64ee922c655111a8e4cc4683172caaaf65
stephen-1bee <joaofelixo764@gmail.com>
2024-05-23 13:26:10 +0000

    few changes

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -1,74 +1,130 @@
-'use client'
-
-import { Button } from '@nextui-org/button';
-import Image from 'next/image';
-import React, { useState } from 'react'
-import { Toaster, toast } from 'react-hot-toast';
+"use client"
+import { Button } from "@nextui-org/button"
+import Image from "next/image"
+import React, { useState } from "react"
+import { Toaster, toast } from "react-hot-toast"
 
 const Login = () => {
+  const [email, setemail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const [isLoading, setIsLoading] = useState(false)
+
   const toMainPage = () => {
-    setIsLoading(true);
-    toast.success('Login Successful!!')
-    setTimeout(() => {
-        window.location.href = './main';
-    }, 2000); 
-}
+    window.location.href = "/main"
+  }
 
+  const handleLogin = async () => {
+    if (!email || !password) {
+      return toast.error("All fields are required")
+    }
+    try {
+      setIsLoading(true)
+      const myHeaders = new Headers()
+      myHeaders.append("Content-Type", "application/json")
 
-    const [isLoading, setIsLoading] = useState(false);
+      const raw = JSON.stringify({
+        email: email,
+        password: password,
+      })
 
-    const handleLogin = () => {
-      try {
-        
-      } catch (error) {
-        console.log(error)
+      const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow",
       }
-    }
-   
-    const signup = () =>{
-      window.location.href='/signup'
-    }
 
+      await fetch("http://localhost:1000/api/v1/admins/login", requestOptions)
+        .then((response) => response.json())
+        .then((result) => {
+          if (result.msg === "login successful") {
+            toast.success(result.msg)
+            console.log(result.msg)
+            setIsLoading(false)
+            window.location.href = "./main"
+          } else {
+            toast.error(result.msg)
+            setIsLoading(false)
+          }
+        })
+        .catch((error) => console.error(error))
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const signup = () => {
+    window.location.href = "/signup"
+  }
 
   return (
-
-    <div className='flex items-center justify-center gap-[30px]  min-h-[100vh]'>
+    <div className="flex items-center justify-center gap-[30px]  min-h-[100vh]">
       <div>
-          <Image 
+        <Image
           height={300}
           width={300}
-          src='/doc.jpg'
-          alt='a young doctor smiling'
-          className='rounded-[10px] doc'
-          />
+          src="/doc.jpg"
+          alt="a young doctor smiling"
+          className="rounded-[10px] doc"
+        />
       </div>
-        <div className='bg-white p-10 rounded-[10px] shadow-lg flex flex-col gap-[12px] items-center form'>
-          <div className='flex flex-col items-center'>
-            <h1 className='text-[20px]  font-bold '>G_TEN HOSPITAL</h1>
-          <h1 className='text-[15px] text-[dodgerblue] font-bold '>ADMIN PORTAL</h1>
-          </div>
-         
-          <form action="" className='flex flex-col  gap-[12px]'>
-            
-              <div className='flex flex-col gap-[5px]'>
-                <label htmlFor="" className='text-[13px]'>Username</label>
-                <input className='text-[13px] p-[10px] border-dashed outline-none bg-[transparent] w-[16vw] border-[#ccc] border-[2px] rounded-[10px]' type="text" placeholder='Enter your username' />
-              </div>
-              <div className='flex flex-col gap-[5px]'>
-                <label htmlFor="" className='text-[13px]'>Password</label>
-                <input className='text-[13px] p-[10px] border-dashed outline-none bg-[transparent] w-[16vw] border-[#ccc] border-[2px] rounded-[10px]' type="password" placeholder='Enter your password' />
-              </div>
-          </form>
-          <div onClick={() => toMainPage()}> 
-          <button  className='bg-[dodgerblue] hover:bg-[#4482d4] mt-[20px] text-[13px] items-center flex justify-center p-[10px] w-[16vw] text-white rounded-[10px]'>{isLoading? <img className='h-[3vh] ' src='/loading.gif' alt='loading' /> : 'Login'}</button>
-          </div>
-           
-            <div className='flex gap-[5px]'>
-              <p className='text-[13px]'>Are you a new Admin?</p> 
-              <p className='text-[dodgerblue] text-[13px] cursor-pointer' onClick={()=> signup()}>Sign up</p>
-            </div>
+      <div className="bg-white p-10 rounded-[10px] shadow-lg flex flex-col gap-[12px] items-center form">
+        <div className="flex flex-col items-center">
+          <h1 className="text-[20px]  font-bold ">G_TEN HOSPITAL</h1>
+          <h1 className="text-[15px] text-[dodgerblue] font-bold ">
+            ADMIN PORTAL
+          </h1>
         </div>
-       <Toaster />
+
+        <form action="" className="flex flex-col  gap-[12px]">
+          <div className="flex flex-col gap-[5px]">
+            <label htmlFor="" className="text-[13px]">
+              Username
+            </label>
+            <input
+              onChange={(e) => setemail(e.target.value)}
+              className="text-[13px] p-[10px] border-dashed outline-none bg-[transparent] w-[16vw] border-[#ccc] border-[2px] rounded-[10px]"
+              type="text"
+              placeholder="Enter your username"
+            />
+          </div>
+          <div className="flex flex-col gap-[5px]">
+            <label htmlFor="" className="text-[13px]">
+              Password
+            </label>
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              className="text-[13px] p-[10px] border-dashed outline-none bg-[transparent] w-[16vw] border-[#ccc] border-[2px] rounded-[10px]"
+              type="password"
+              placeholder="Enter your password"
+            />
+          </div>
+        </form>
+        <div>
+          <button
+            onClick={() => handleLogin()}
+            className="bg-[dodgerblue] hover:bg-[#4482d4] mt-[20px] text-[13px] items-center flex justify-center p-[10px] w-[16vw] text-white rounded-[10px]"
+          >
+            {isLoading ? (
+              <img className="h-[3vh] " src="/loading.gif" alt="loading" />
+            ) : (
+              "Login"
+            )}
+          </button>
+        </div>
+
+        <div className="flex gap-[5px]">
+          <p className="text-[13px]">Are you a new Admin?</p>
+          <p
+            className="text-[dodgerblue] text-[13px] cursor-pointer"
+            onClick={() => signup()}
+          >
+            Sign up
+          </p>
+        </div>
+      </div>
+      <Toaster />
     </div>
   )
 }
--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -1,112 +1,138 @@
-'use client'
-import { Image } from '@nextui-org/react'
-import React, { useState } from 'react'
-import toast, { Toaster } from 'react-hot-toast'
+"use client"
+import { Image } from "@nextui-org/react"
+import React, { useState } from "react"
+import toast, { Toaster } from "react-hot-toast"
 
 const page = () => {
   const [email, setemail] = useState("")
   const [password, setPassword] = useState("")
   const [name, setName] = useState("")
 
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false)
 
   // handle sign up
-  const signup = async () =>{
-    if(!name || !email || !password) {
-     return toast.error("All fields are required")
+  const signup = async () => {
+    if (!name || !email || !password) {
+      return toast.error("All fields are required")
     }
     try {
-    setIsLoading(true)
-        const myHeaders = new Headers();
-         myHeaders.append("Content-Type", "application/json");
+      setIsLoading(true)
+      const myHeaders = new Headers()
+      myHeaders.append("Content-Type", "application/json")
 
-    const raw = JSON.stringify({
-      "name": name,
-      "email": email,
-      "password": password
-    });
-
-    const requestOptions = {
-      method: "POST",
-      headers: myHeaders,
-      body: raw,
-      redirect: "follow"
-    };
-
-    await fetch("http://localhost:1000/api/v1/admins/create", requestOptions)
-      .then((response) => response.json())
-      .then((result) => {
-        if(result.msg === "Sign up successful"){
-          setIsLoading(true)
-          toast.success(result.msg)
-        console.log(result.msg)
-        setIsLoading(false)
-        }else{
-          toast.error(result.msg)
-          setIsLoading(false)
-        }
+      const raw = JSON.stringify({
+        name: name,
+        email: email,
+        password: password,
       })
-      .catch((error) => console.error(error));
-  } catch (err) {
-    console.log(err);
-  }  
+
+      const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow",
+      }
+
+      await fetch("http://localhost:1000/api/v1/admins/create", requestOptions)
+        .then((response) => response.json())
+        .then((result) => {
+          if (result.msg === "Sign up successful") {
+            setIsLoading(true)
+            toast.success(result.msg)
+            console.log(result.msg)
+            setIsLoading(false)
+            window.location.href = "/"
+          } else {
+            toast.error(result.msg)
+            setIsLoading(false)
+          }
+        })
+        .catch((error) => console.error(error))
+    } catch (err) {
+      console.log(err)
+    }
   }
 
-  console.log(email);
-  console.log(password);
-  console.log(name);
-
   const toLoginPage = () => {
-    window.location.href = '/';
+    window.location.href = "/"
   }
 
   return (
-    <div className='min-h-screen flex items-center justify-center sign'>
-        <div className='flex items-center gap-[2rem]'>
-            <div className='image'>
-                <Image 
-                width={300}
-                height={300}
-                src='/fdoc.jpg'
-                alt='female doctor'
-                />
-            </div>
-            <div className='bg-white h-[70vh] p-10 rounded-[10px] shadow-lg flex flex-col gap-[12px] items-center forms'>
-              <div className='flex flex-col items-center'>
-                <h1 className='text-[20px]  font-bold '>G_TEN HOSPITAL</h1>
-                <h1 className='text-[15px] text-[dodgerblue] font-bold '>ADMIN PORTAL</h1>
-              </div>
-         
-            <form action="" className='flex flex-col  gap-[12px]'>
-            
-              <div className='flex flex-col gap-[5px]'>
-                <label htmlFor="" className='text-[13px]'>Username</label>
-                <input onChange={(e) => setName(e.target.value)} className='text-[13px] p-[10px] border-dashed outline-none bg-[transparent] w-[16vw] border-[#ccc] border-[2px] rounded-[10px]' type="text" placeholder='Enter your username' />
-              </div>
-
-              <div className='flex flex-col gap-[5px]'>
-                <label htmlFor="" className='text-[13px]'>Email</label>
-                <input onChange={(e) => setemail(e.target.value)} className='text-[13px] p-[10px] border-dashed outline-none bg-[transparent] w-[16vw] border-[#ccc] border-[2px] rounded-[10px]' type="text" placeholder='Enter your email' />
-              </div>
-
-              <div className='flex flex-col gap-[5px]'>
-                <label htmlFor="" className='text-[13px]'>Password</label>
-                <input onChange={(e) => setPassword(e.target.value)} className='text-[13px] p-[10px] border-dashed outline-none bg-[transparent] w-[16vw] border-[#ccc] border-[2px] rounded-[10px]' type="password" placeholder='Enter your password' />
-              </div>
-            </form>
-          <div > 
-             <button onClick={()=> signup()}  className='bg-[dodgerblue] hover:bg-[#4482d4] mt-[20px] text-[13px] items-center flex justify-center p-[10px] w-[16vw] text-white rounded-[10px]'>{isLoading? <img className='h-[3vh] ' src='/loading.gif' alt='loading' /> : 'signup'}</button>
+    <div className="min-h-screen flex items-center justify-center sign">
+      <div className="flex items-center gap-[2rem]">
+        <div className="image">
+          <Image width={300} height={300} src="/fdoc.jpg" alt="female doctor" />
+        </div>
+        <div className="bg-white h-[70vh] p-10 rounded-[10px] shadow-lg flex flex-col gap-[12px] items-center forms">
+          <div className="flex flex-col items-center">
+            <h1 className="text-[20px]  font-bold ">G_TEN HOSPITAL</h1>
+            <h1 className="text-[15px] text-[dodgerblue] font-bold ">
+              ADMIN PORTAL
+            </h1>
           </div>
-           
-            <div className='flex gap-[5px]'>
-              <p className='text-[13px]'>Already an admin?</p> 
-              <p className='text-[dodgerblue] text-[13px] cursor-pointer' onClick={() => toLoginPage()} >Login</p>
+
+          <form action="" className="flex flex-col  gap-[12px]">
+            <div className="flex flex-col gap-[5px]">
+              <label htmlFor="" className="text-[13px]">
+                Username
+              </label>
+              <input
+                onChange={(e) => setName(e.target.value)}
+                className="text-[13px] p-[10px] border-dashed outline-none bg-[transparent] w-[16vw] border-[#ccc] border-[2px] rounded-[10px]"
+                type="text"
+                placeholder="Enter your username"
+              />
             </div>
-            
+
+            <div className="flex flex-col gap-[5px]">
+              <label htmlFor="" className="text-[13px]">
+                Email
+              </label>
+              <input
+                onChange={(e) => setemail(e.target.value)}
+                className="text-[13px] p-[10px] border-dashed outline-none bg-[transparent] w-[16vw] border-[#ccc] border-[2px] rounded-[10px]"
+                type="text"
+                placeholder="Enter your email"
+              />
+            </div>
+
+            <div className="flex flex-col gap-[5px]">
+              <label htmlFor="" className="text-[13px]">
+                Password
+              </label>
+              <input
+                onChange={(e) => setPassword(e.target.value)}
+                className="text-[13px] p-[10px] border-dashed outline-none bg-[transparent] w-[16vw] border-[#ccc] border-[2px] rounded-[10px]"
+                type="password"
+                placeholder="Enter your password"
+              />
+            </div>
+          </form>
+          <div>
+            <button
+              onClick={() => signup()}
+              className="bg-[dodgerblue] hover:bg-[#4482d4] mt-[20px] text-[13px] items-center flex justify-center p-[10px] w-[16vw] text-white rounded-[10px]"
+            >
+              {isLoading ? (
+                <img className="h-[3vh] " src="/loading.gif" alt="loading" />
+              ) : (
+                "signup"
+              )}
+            </button>
+          </div>
+
+          <div className="flex gap-[5px]">
+            <p className="text-[13px]">Already an admin?</p>
+            <p
+              className="text-[dodgerblue] text-[13px] cursor-pointer"
+              onClick={() => toLoginPage()}
+            >
+              Login
+            </p>
+          </div>
         </div>
-      
-        </div>
-        <Toaster />
+      </div>
+      <Toaster />
     </div>
   )
 }
